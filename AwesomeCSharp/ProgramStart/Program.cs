d10d9763fe2f594b0459cf1b5d4483367d442f14
rsk2 <beingrohan24.7@gmail.com>
2020-05-09 19:26:20 +0530

    Adding HashSet demo

--- AwesomeCSharp/ProgramStart/Program.cs
+++ AwesomeCSharp/ProgramStart/Program.cs
@@ -12,6 +12,7 @@ namespace ProgramStart
             DemonstrateStringInterpolation();
             DemonstrateRefVsOut();
             DemonstrateAnonymousFunction();
+            DemonstrateHashSet();
             Console.ReadLine();
         }
 
@@ -21,7 +22,6 @@ namespace ProgramStart
             //String.Join provides us more readable syntax and we don't need to write a loop
             var intArray = new int[] { 1, 2, 3, 4, 10 };
             Console.WriteLine("Integer array: ({0})", string.Join(",", intArray));
-
             //Below lines show that we can use String.Join with list 
             var floatList = new List<float> { 1.3f, 2.0f, 4.5f, 1.22f, 7.8f};
             Console.WriteLine("List of fractions: ({0})", string.Join("|", floatList));
@@ -60,13 +60,24 @@ namespace ProgramStart
         public static void DemonstrateAnonymousFunction()
         {
             Console.WriteLine("Demonstrating AnonymousFunction:");
-            GenerateInteger Zero = new GenerateInteger( () => 0);
+            GenerateInteger Zero = new GenerateInteger(() => 0);
             Console.WriteLine("Generate integer Zero() generates: " + Zero());
             GenerateInteger RandomInteger = new GenerateInteger(() => (new Random()).Next());
             Console.WriteLine("Generate integer RandomInteger() generates: " + RandomInteger());
             GenerateInteger CurrentMonth = new GenerateInteger(() => DateTime.Now.Month);
             Console.WriteLine("Generate integer CurrentMonth() generates: " + CurrentMonth());
             Console.WriteLine();
+        }
+
+        //C# has inbuilt support for HashSet, HashTables and Dictionaries. 
+        public static void DemonstrateHashSet()
+        {
+            Console.WriteLine("Demonstrating HashSet:");
+            int[] arrayWithDuplicates = { 1, -11, 44, 101, 1, 3, 54, 44, -2, -11 };
+            Console.WriteLine($"Original Array: {String.Join(",", arrayWithDuplicates)}");
+            int[] arrayWithoutDuplicates = RemoveDuplicates(arrayWithDuplicates);
+            Console.WriteLine($"Array w/o duplicates (created using HashSet): {String.Join(",", arrayWithoutDuplicates)}");
+
         }
 
         #region Helpers
@@ -77,6 +88,24 @@ namespace ProgramStart
             j = i;
             i = 0;
         }
+
+        static int[] RemoveDuplicates(int[] original)
+        {
+            if (original.Length <= 1)
+                return original;
+            HashSet<int> hs = new HashSet<int>();
+            List<int> result = new List<int>();
+
+            foreach (int x in original)
+            {
+                if (!hs.Contains(x))
+                    result.Add(x);
+                         
+                hs.Add(x);
+            }
+            return result.ToArray();
+        }
+
         #endregion
 
     }
